23ec3931e37ca6b36382b15fb8bb227198d0ede2
return0927 <bjleh0927@gmail.com>
2017-07-16 13:42:22 +0900

    Discord ] Setting manager Load

---
+++ Discord/setting.py
@@ -0,0 +1,16 @@
+import json, codecs
+
+class settings:
+    def get():
+        with codecs.open("settings.json", "r", encoding="UTF-8") as f:
+            string = " ".join([str(x).replace("\r", "").replace("\n", "").replace("\t", "") for x in f.readlines()])
+        return json.loads(string)
+
+    def save(string):
+        try:
+            with codecs.open("settings.json", "w", encoding='UTF-8') as f:
+                f.write(string)
+            return None
+
+        except Exception as ex:
+            return str(ex)
